9b3af5b053624f341bf2126cf45c832f5635f234
Matteus Moreno <matteus_moreno@live.com>
2024-10-02 08:55:41 -0300

    initial commit

---
+++ src/main/java/br/com/matteusmoreno/uol_backend_challenge/UolBackendChallengeApplication.java
@@ -0,0 +1,13 @@
+package br.com.matteusmoreno.uol_backend_challenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UolBackendChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UolBackendChallengeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/matteusmoreno/uol_backend_challenge/UolBackendChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.matteusmoreno.uol_backend_challenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UolBackendChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
