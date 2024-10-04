432d0eb7c4d6390fd108ebb709200d1e3595fbb5
Matteus Moreno <matteus_moreno@live.com>
2024-10-04 13:16:33 -0300

    adicionando autorização para o endereço do frontend da Azure

--- src/main/java/br/com/matteusmoreno/uol_backend_challenge/config/WebConfig.java
+++ src/main/java/br/com/matteusmoreno/uol_backend_challenge/config/WebConfig.java
@@ -11,7 +11,8 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins(
-                        "http://localhost:8000")
+                        "http://localhost:8000",
+                        "https://wonderful-moss-03892360f.5.azurestaticapps.net")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
